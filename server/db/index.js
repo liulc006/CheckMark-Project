31e87095826e76ec474a8fde4dba5a4b3f35badf
Luca Liu <lucaliu96@gmail.com>
2023-03-29 22:25:18 -0400

    added Checklists collections to MongoDB

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('./model/Users');
+const Checklist = require('./model/Checklists');
 
 const dB = process.env.DATABASE_URL || 'mongodb://localhost:27017/checkmark-app';
 
@@ -13,13 +14,37 @@ const conn = mongoose.connect(dB, {
 const syncAndSeed= async () => {
     try{
         await mongoose.connection.dropDatabase();
-        await Promise.all([
+        const [luca] = await Promise.all([
             User.create({
                 firstName: "Luca",
                 lastName: "Liu",
                 email: "luca@gmail.com",
                 password: "123"
             })
+        ]);
+
+        await Promise.all([
+            Checklist.create({
+                userId: luca._id,
+                priorityLevel: 'high',
+                tags: ['Grocery', 'Dinner'],
+                status: 'close',
+                description: 'Need to buy Scallion, Beef and Soy Sauce'
+            }),
+            Checklist.create({
+                userId: luca._id,
+                priorityLevel: 'medium',
+                tags: ['Health'],
+                status: 'open',
+                description: 'Run across the bridge'
+            }),            
+            Checklist.create({
+                userId: luca._id,
+                priorityLevel: 'low',
+                tags: ['Chores'],
+                status: 'open',
+                description: 'Wash the dishes'
+            }),
         ]);
     }
     catch(err){
---
+++ server/db/model/Checklists.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose');
+
+const checklistSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    priorityLevel: {
+        type: String,
+        enum: ['high', 'medium', 'low'],
+        required: true,
+        default: 'low'
+    },
+    tags: [ String ],
+    description: String,
+    status: {
+        type: String,
+        enum: ['open', 'close'],
+        required: true,
+        default: 'open'
+    }
+});
+
+const Checklist = model('Checklist', checklistSchema);
+
+module.exports = Checklist;
