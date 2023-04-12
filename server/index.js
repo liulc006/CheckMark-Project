cd23f5049e5901e4d65c83e2b2d45d7f2be58b12
Luca Liu <lucaliu96@gmail.com>
2023-04-12 10:51:06 -0400

    moved html to static

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json({limit: '50 mb'}));
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use('/static', express.static(path.join(__dirname, '../static')));
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../src/index.html')));
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
 
 app.use('/api/auth', require('./api/auth'));
 app.use('/api/checklist', require('./api/checklist'));
